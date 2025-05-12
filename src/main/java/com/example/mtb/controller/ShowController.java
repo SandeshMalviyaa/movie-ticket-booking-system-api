dbccefb81bb6c4e83ee1ac3ffb684ddd841f477a
sandeshmalviya <sandeshmalviya09602@gmail.com>
2025-05-12 17:36:08 +0530

    show created

--- src/main/java/com/example/mtb/controller/ShowController.java
+++ src/main/java/com/example/mtb/controller/ShowController.java
@@ -1,11 +1,17 @@
 package com.example.mtb.controller;
 
+import com.example.mtb.dto.ShowResponse;
 import com.example.mtb.util.ResponseStructure;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.example.mtb.service.ShowService;
 import com.example.mtb.util.RestResponseBuilder;
 import lombok.AllArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +22,10 @@ public class ShowController {
     private final ShowService showService;
     private final RestResponseBuilder responseBuilder;
 
-//    public ResponseEntity<ResponseStructure<ShowRe>>
+    @PostMapping("theaters/{theaterId}/screens/{screenId}/shows")
+    @PreAuthorize("hasAuthority('THEATER_OWNER')")
+    public ResponseEntity<ResponseStructure<ShowResponse>> addShow(@PathVariable  String theaterId, @PathVariable String screenId, String movieId , @NotNull Long startTime ){
+        ShowResponse showResponse = showService.addShow(theaterId, screenId, movieId, startTime);
+        return responseBuilder.sucess(HttpStatus.OK, "Show sucessfully created", showResponse);
+    }
 }
---
+++ src/main/java/com/example/mtb/repository/ShowRepository.java
@@ -0,0 +1,8 @@
+package com.example.mtb.repository;
+
+import com.example.mtb.entity.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ShowRepository extends JpaRepository<Show,String> {
+
+}
